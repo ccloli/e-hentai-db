0397f10966b6cbf94a9f558512cefd92d172c1d7
ccloli <h@ccloli.com>
2019-08-03 15:03:24 +0800

    remove dummy

--- app/router/api/gallery.js
+++ app/router/api/gallery.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const gallery = require('../../action/gallery');
 const catchError = require('../../util/catchError');
 
-const router = Router({ mergeParams: true });
+const router = Router();
 router.use('/:gid?/:token?', catchError(gallery));
 
 module.exports = router;
