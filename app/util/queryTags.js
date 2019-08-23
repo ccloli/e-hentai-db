8aec3349fa6fdf83602366f5ab47fa553edd5460
ccloli <h@ccloli.com>
2019-08-24 00:32:05 +0800

    add torrents into search result

---
+++ app/util/queryTags.js
@@ -0,0 +1,15 @@
+const queryTags = async (conn, gids) => {
+	const gidTags = {};
+	const tagResult = await conn.query(
+		'SELECT a.gid, b.name FROM gid_tid AS a INNER JOIN tag AS b ON a.tid = b.id WHERE a.gid IN (?)', [gids]
+	);
+	tagResult.forEach(({ gid, name }) => {
+		if (!gidTags[gid]) {
+			gidTags[gid] = [];
+		}
+		gidTags[gid].push(name);
+	});
+	return gidTags;
+};
+
+module.exports = queryTags;
---
+++ app/util/queryTorrents.js
@@ -0,0 +1,15 @@
+const queryTorrents = async (conn, gids) => {
+	const gidTorrents = {};
+	const torrentResult = await conn.query(
+		'SELECT * FROM torrent WHERE gid IN (?)', [gids]
+	);
+	torrentResult.forEach(({ gid, ...rest }) => {
+		if (!gidTorrents[gid]) {
+			gidTorrents[gid] = [];
+		}
+		gidTorrents[gid].push(rest);
+	});
+	return gidTorrents;
+};
+
+module.exports = queryTorrents;
